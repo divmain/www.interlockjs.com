501a0fcb865c9eac3a71eb32b0df3c38fdf6e507
Dale Bustad <dale@divmain.com>
2016-07-31 20:22:44 -0700

    Scrolling and structure for the Extensiblity doc page.

--- public/_js/scroll.js
+++ public/_js/scroll.js
@@ -6,7 +6,8 @@
   var i = 0;
   function scrollTo (targetSelector, duration, cb) {
     return function (ev) {
-      if (ev) { ev.preventDefault(); }
+      var header = document.querySelector("body > .header");
+      var headerHeight = header && header.offsetHeight || 0;
 
       var scrollIdx = ++i;
       var target = document.querySelector(targetSelector);
@@ -14,8 +15,7 @@
       var end = start + duration;
 
       var startPos = document.body.scrollTop;
-      var endPos = target.offsetTop;
-      console.log("end position of", endPos, "for", target);
+      var endPos = target.offsetTop - headerHeight;
       var difference = endPos - startPos;
 
       window.requestAnimationFrame(function step () {
@@ -23,8 +23,6 @@
         if (scrollIdx !== i) { return; }
 
         var newPos = startPos + difference * ease(start, end, Date.now());
-        console.log("setting document.body.scrollTop to", newPos);
-        console.log("but the target offset is", target.offsetTop);
         document.body.scrollTop = document.documentElement.scrollTop = newPos;
         if (newPos !== endPos) {
           window.requestAnimationFrame(step);
@@ -47,22 +45,18 @@
       (1 - midpointCoefficientR * Math.pow(1 - progress, EASING_POWER));
   }
 
-  // Setup scrolling for all in-page links.
-  var links = document.querySelectorAll("a");
-  Array.prototype.forEach.call(links, function (el) {
-    if (el.baseURI === document.location.href && el.hash) {
-      el.addEventListener("click", scrollTo(el.hash, ANIMATION_DURATION));
+  window.addEventListener("load", function () {
+    // Setup scrolling for all in-page links.
+    document.querySelectorAll("a").forEach(function (el) {
+      console.log(el.baseURI, document.location.href, el.hash)
+      if (el.baseURI === document.location.href && el.hash) {
+        el.addEventListener("click", scrollTo(el.hash, ANIMATION_DURATION));
+      }
+    });
+
+    // Scroll to the correct position on page load.
+    if (document.location.hash) {
+      scrollTo(document.location.hash, ANIMATION_DURATION)();
     }
   });
-
-  // Scroll to the correct position on page load.
-  if (document.location.hash) {
-    window.addEventListener(
-      "load",
-      setTimeout(function () {
-        scrollTo(document.location.hash, ANIMATION_DURATION)();
-      }, 0),
-      false
-    );
-  }
 })();
