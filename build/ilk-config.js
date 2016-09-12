f1623d70380316d0961599d8e1f59cda72f93e09
Dale Bustad <dale@divmain.com>
2016-09-11 21:12:39 -0700

    Extract the helper functions into separate module; add raw-file support.

--- build/ilk-config.js
+++ build/ilk-config.js
@@ -1,61 +1,45 @@
 const path = require("path");
 const glob = require("glob");
+const { generateEntries, generateRawEntries } = require("./helpers");
 const jadePlugin = require("./jade-plugin");
 const stylusPlugin = require("./stylus-plugin");
 const interlockHtml = require("interlock-html");
 const interlockCss = require("interlock-css");
+const interlockRaw = require("interlock-raw");
 
 
 const srcRoot = path.resolve(__dirname, "../src");
 const destRoot = path.resolve(__dirname, "../dist")
 
-const jsFiles = filterHidden(glob.sync("**/*.js", { cwd: srcRoot }));
-const jadeFiles = filterHidden(glob.sync("**/*.jade", { cwd: srcRoot }));
-const htmlFiles = filterHidden(glob.sync("**/*.{htm,html}", { cwd: srcRoot }));
-const stylusFiles = filterHidden(glob.sync("**/*.styl", { cwd: srcRoot }));
-const cssFiles = filterHidden(glob.sync("**/*.css", { cwd: srcRoot }));
+const excludeFromRaw = ["js", "jade", "html", "styl", "css"];
 
-const jsEntries = jsFiles.reduce((memo, relPath) => {
-  memo[relPath] = relPath;
-  return memo;
-}, {});
-const jadeEntries = jadeFiles.reduce((memo, relPath) => {
-  memo[relPath] = relPath.replace(/\.jade$/, ".html");
-  return memo;
-}, {});
-const stylusEntries = stylusFiles.reduce((memo, relPath) => {
-  memo[relPath] = relPath.replace(/\.styl$/, ".css");
-  return memo;
-}, {});
-const cssEntries = cssFiles.reduce((memo, relPath) => {
-  memo[relPath] = relPath;
-  return memo;
-}, {});
+const jsEntries = generateEntries(srcRoot, "js", null);
+const jadeEntries = generateEntries(srcRoot, "jade", "html");
+const htmlEntries = generateEntries(srcRoot, "html", null);
+const stylusEntries = generateEntries(srcRoot, "styl", "css");
+const cssEntries = generateEntries(srcRoot, "css", null);
+const rawEntries = generateRawEntries(srcRoot, excludeFromRaw);
+
 
 module.exports = {
   srcRoot,
   destRoot,
 
-  entry: Object.assign({}, jsEntries, jadeEntries, stylusEntries, cssEntries),
+  entry: Object.assign(
+    {},
+    jsEntries,
+    htmlEntries,
+    jadeEntries,
+    stylusEntries,
+    cssEntries,
+    rawEntries
+  ),
 
   plugins: [
     jadePlugin,
     stylusPlugin,
     interlockHtml(),
-    interlockCss()
-  ],
-
-  // babelConfig: {
-  //   plugins: [require.resolve("./example-plugin")]
-  // }
+    interlockCss(),
+    interlockRaw({ exclude: excludeFromRaw })
+  ]
 };
-
-function filterHidden (relPaths) {
-  return relPaths.filter(relPath => {
-    const pathSegments = relPath.split("/");
-    return pathSegments.reduce(
-      (memo, segment) => memo && segment.indexOf("_") !== 0,
-      true
-    );
-  });
-}
