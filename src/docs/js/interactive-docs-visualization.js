98f4c99ffc7926cccd70f257ecae8a0fbeb288b6
Dale Bustad <dale@divmain.com>
2016-09-12 22:49:49 -0700

    Load libs through bundle; remove <script> tags.  Upgrade D3 to v4.

--- src/docs/js/interactive-docs-visualization.js
+++ src/docs/js/interactive-docs-visualization.js
@@ -1,47 +1,50 @@
+import marked from "marked";
+import { scaleLinear, scalePow } from "d3-scale";
+import { select } from "d3-selection";
+import "d3-transition";
+import { hierarchy, partition } from "d3-hierarchy";
+import { arc } from "d3-shape";
+import { interpolate } from "d3-interpolate";
+
+
+const JSON_PATH = "http://rawgit.com/interlockjs/interlock/master/docs/compilation.json";
+const width = 700;
+const height = width / 2;
+const radius = height;
+const colors = ["#188dc0", "#21c5b5", "#2a8eff", "#24c7e5", "#17a8fd", "#1bc0fd", "#1c9bb5", "#31b1b5", "#318ed5", "#2c8fea", "#2faad6", "#18a5e5", "#16c6d3", "#16b0c5", "#28c2ef", "#28c4c2", "#2191b6", "#26b1d3", "#2eb8ff", "#1596ff", "#27a0ff", "#1994d3", "#1797ea", "#2da0ea", "#179ed2", "#17afe7", "#229fc4", "#2ea4b5", "#30c1d6", "#17acf9", "#20b7c3", "#2b9bc6", "#2da9c3", "#1ab4df", "#15b8d5", "#15bec3", "#1abbf0", "#19bde2", "#308dde", "#2f8df1", "#30aab5", "#18b4f4", "#21bfb5", "#2e8db7", "#308ec6", "#2ab8b5", "#2d9cd6", "#1fc7fc", "#3097b5", "#2797f8", "#319ee0", "#2e97e3", "#29abd1", "#2facf2", "#228dcf", "#1693c2", "#168df8", "#1d95dd", "#2d97cb", "#1898f3", "#18a3c3", "#219ff1", "#15a3dd", "#22a1fa", "#23b2e4", "#2da9dc", "#1dc6ee", "#30adea", "#2cb2cb", "#1692cb", "#22a1b7", "#1ca2cc", "#1fa4eb", "#1caebd", "#15b5b9", "#1bb9cb", "#2eb7f1", "#2dbfca", "#15c6cd", "#23c7bf", "#31c3fb", "#1f8edc", "#1d8eef", "#1790e8", "#1797ba", "#1caeb7", "#29a8f8", "#31b8cd", "#31b9df", "#26bcfe", "#15c5d8", "#3195d7", "#2299c0", "#279dbc", "#179ddb", "#19a1d1", "#16a8bb", "#31abcb", "#18aef0", "#18b3fd", "#22b9e8", "#30bddc", "#1bc1db", "#3191c5", "#2f92c2", "#2a93bc", "#2f9aca", "#30a8df", "#2dacc2", "#31b6be", "#2abdbe", "#29beb8", "#19bfcf", "#2dc1f2", "#2ac6e8", "#1f8ecb", "#2994b5", "#1e91e3", "#2292dc", "#1c90f8", "#1792f0", "#2597d2", "#1794f9", "#1699de", "#1e9ad1", "#1a9ae8", "#16a1bd", "#2b9af7", "#279bf2", "#30a3bc", "#1fa2e4", "#1aa6c9", "#29a7b6", "#19a3f0", "#2fa4f9", "#15accb", "#2da9f2", "#1face6", "#17ade0", "#1baddb", "#18b1d8", "#31b3bf", "#18b0f6", "#1eb5d4", "#2fb5c7"];
+
 window.addEventListener('load', function() {
-  var JSON_PATH = "http://rawgit.com/interlockjs/interlock/master/docs/compilation.json";
-
-  var md = markdownit();
-
-  var width = 700,
-    height = width / 2,
-    radius = height;
-
-  var x = d3.scale.linear()
+  const x = scaleLinear()
     .range([0, Math.PI]);
 
-  var y = d3.scale.pow()
+  const y = scalePow()
     .exponent(0.7)
     .range([0, radius]);
 
-  var colorIdx = 0;
-  function getColor () {
-    return colors[colorIdx++];
-  }
-  var colors = ["#188dc0", "#21c5b5", "#2a8eff", "#24c7e5", "#17a8fd", "#1bc0fd", "#1c9bb5", "#31b1b5", "#318ed5", "#2c8fea", "#2faad6", "#18a5e5", "#16c6d3", "#16b0c5", "#28c2ef", "#28c4c2", "#2191b6", "#26b1d3", "#2eb8ff", "#1596ff", "#27a0ff", "#1994d3", "#1797ea", "#2da0ea", "#179ed2", "#17afe7", "#229fc4", "#2ea4b5", "#30c1d6", "#17acf9", "#20b7c3", "#2b9bc6", "#2da9c3", "#1ab4df", "#15b8d5", "#15bec3", "#1abbf0", "#19bde2", "#308dde", "#2f8df1", "#30aab5", "#18b4f4", "#21bfb5", "#2e8db7", "#308ec6", "#2ab8b5", "#2d9cd6", "#1fc7fc", "#3097b5", "#2797f8", "#319ee0", "#2e97e3", "#29abd1", "#2facf2", "#228dcf", "#1693c2", "#168df8", "#1d95dd", "#2d97cb", "#1898f3", "#18a3c3", "#219ff1", "#15a3dd", "#22a1fa", "#23b2e4", "#2da9dc", "#1dc6ee", "#30adea", "#2cb2cb", "#1692cb", "#22a1b7", "#1ca2cc", "#1fa4eb", "#1caebd", "#15b5b9", "#1bb9cb", "#2eb7f1", "#2dbfca", "#15c6cd", "#23c7bf", "#31c3fb", "#1f8edc", "#1d8eef", "#1790e8", "#1797ba", "#1caeb7", "#29a8f8", "#31b8cd", "#31b9df", "#26bcfe", "#15c5d8", "#3195d7", "#2299c0", "#279dbc", "#179ddb", "#19a1d1", "#16a8bb", "#31abcb", "#18aef0", "#18b3fd", "#22b9e8", "#30bddc", "#1bc1db", "#3191c5", "#2f92c2", "#2a93bc", "#2f9aca", "#30a8df", "#2dacc2", "#31b6be", "#2abdbe", "#29beb8", "#19bfcf", "#2dc1f2", "#2ac6e8", "#1f8ecb", "#2994b5", "#1e91e3", "#2292dc", "#1c90f8", "#1792f0", "#2597d2", "#1794f9", "#1699de", "#1e9ad1", "#1a9ae8", "#16a1bd", "#2b9af7", "#279bf2", "#30a3bc", "#1fa2e4", "#1aa6c9", "#29a7b6", "#19a3f0", "#2fa4f9", "#15accb", "#2da9f2", "#1face6", "#17ade0", "#1baddb", "#18b1d8", "#31b3bf", "#18b0f6", "#1eb5d4", "#2fb5c7"];
+  let colorIdx = 0;
+  const getColor = () => colors[colorIdx++];
 
-  var svg = d3.select(".interactive-docs .visualization svg")
-    .on("mouseleave", clearStatusBar)
+  const svg = select(".interactive-docs .visualization svg")
     .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height + ")");
 
-  var statusBar = d3.select(".interactive-docs .status");
+  svg.on("mouseleave", clearStatusBar)
 
-  var doc = d3.select(".doc");
+  const statusBar = select(".interactive-docs .status");
 
-  var partition = d3.layout.partition()
-    .sort(function (a, b) { return b.position - a.position; })
-    .value(function(d) { return d.size; });
+  const doc = select(".doc");
 
-  var arc = d3.svg.arc()
-    .startAngle(function(d) { return (Math.PI / 2) - Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-    .endAngle(function(d) { return (Math.PI / 2) - Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+  const p = partition();
+
+  const arcShape = arc()
+    .startAngle(function(d) { return (Math.PI / 2) - Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return (Math.PI / 2) - Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
 
   function updateStatusBar (d) {
     statusBar.html(function () {
-      return d.name;
+      return d.data.name;
     });
   }
 
@@ -51,13 +54,11 @@ window.addEventListener('load', function() {
     });
   }
 
-  d3.json(JSON_PATH, function(err, root) {
-    if (err) throw err;
-
+  fetch(JSON_PATH).then(response => response.json()).then(root => {
     function orderChildren (node, anscestors) {
       if (node.children) {
         node.children = node.children.map(function (child, idx) {
-          var childAnscestors = anscestors.concat(node);
+          const childAnscestors = anscestors.concat(node);
           child = orderChildren(child, childAnscestors);
           child.position = idx;
           return child;
@@ -68,7 +69,9 @@ window.addEventListener('load', function() {
       return node;
     }
 
-    root = orderChildren(root, []);
+    root = hierarchy(orderChildren(root, []))
+      .sort((a, b) => b.data.position - a.data.position)
+      .sum(d => d.size);
 
     svg
       .append("circle")
@@ -78,27 +81,31 @@ window.addEventListener('load', function() {
       .style("stroke", "#121212")
       .style("stroke-dasharray", "2,6")
 
-    var path = svg.selectAll("path")
-      .data(partition.nodes(root))
-      .enter().append("path")
-      .attr("d", arc)
+    const path = svg.selectAll("path")
+      .data(p(root).descendants())
+      .enter()
+      .append("path")
+    path
+      .attr("d", arcShape)
       .style("fill", function (d) { return getColor(); })
       .classed("graph-arc", true)
       .attr("stroke-width", 2)
       .attr("stroke", "#f0f0f0")
-      .attr("stroke", "#121212")
+      .attr("stroke", "#121212");
+
+    path
       .on("click", click)
       .on("mouseenter", updateStatusBar)
       .on("mouseleave", clearStatusBar);
 
     function buildBreadcrumb (nodes, selector) {
       window.requestAnimationFrame(function () {
-        var breadcrumb = document.createDocumentFragment();
+        const breadcrumb = document.createDocumentFragment();
 
         nodes.forEach(function (node, idx) {
-          var isLast = idx === nodes.length - 1;
+          const isLast = idx === nodes.length - 1;
 
-          var domNode = document.createElement("a");
+          const domNode = document.createElement("a");
           domNode.className = "breadcrumb-item";
           domNode.addEventListener("click", function (ev) {
             ev.preventDefault();
@@ -107,41 +114,39 @@ window.addEventListener('load', function() {
           domNode.innerHTML = node.name;
           breadcrumb.appendChild(domNode);
 
-          var separator = document.createElement("i");
+          const separator = document.createElement("i");
           separator.className = "breadcrumb-separator ion-android-arrow-dropright";
           breadcrumb.appendChild(separator);
         });
 
-        var parent = document.querySelector(selector);
+        const parent = document.querySelector(selector);
         parent.innerHTML = "";
         parent.appendChild(breadcrumb);
       });
     }
 
     function updateDoc (d) {
-      doc.html(function () {
-        return md.render(d.markdown);
-      });
-      console.log(d.anscestors);
-      buildBreadcrumb(d.anscestors, ".doc-wrapper .breadcrumb");
+      doc.html(() => marked(d.data.markdown));
+      buildBreadcrumb(d.data.anscestors, ".doc-wrapper .breadcrumb");
     }
 
     function click(d) {
+      updateDoc(d);
+
       path.transition()
         .duration(1200)
         .attrTween("d", arcTween(d));
-      updateDoc(d);
     }
   });
 
   function arcTween(d) {
-    var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-      yd = d3.interpolate(y.domain(), [d.y, 1]),
-      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+    const xd = interpolate(x.domain(), [d.x0, d.x1]),
+      yd = interpolate(y.domain(), [d.y0, 1]),
+      yr = interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
     return function(d, i) {
       return i ?
-        function(t) { return arc(d); } :
-        function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+        function(t) { return arcShape(d); } :
+        function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arcShape(d); };
     };
   }
 }, false);
