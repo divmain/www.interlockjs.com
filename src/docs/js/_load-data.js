6e678e645d2d70fc64fdf8b3a5cd89b1d4b3b26c
Dale Bustad <dale@divmain.com>
2016-09-17 16:04:11 -0700

    Fix ordering and breadcrumb generation for interactive doc.

--- src/docs/js/_load-data.js
+++ src/docs/js/_load-data.js
@@ -4,12 +4,23 @@ import { hierarchy } from "d3-hierarchy";
 const JSON_PATH = "http://rawgit.com/interlockjs/interlock/master/docs/compilation.json";
 
 
-function orderChildren (node, anscestors) {
+function orderChildren (datum) {
+  if (datum.children) {
+    datum.children = datum.children.map((child, idx) => {
+      child = orderChildren(child);
+      child.position = idx;
+      return child;
+    });
+  }
+  return datum;
+}
+
+
+function addAnscestors (node, anscestors) {
   if (node.children) {
     node.children = node.children.map((child, idx) => {
       const childAnscestors = anscestors.concat(node);
-      child = orderChildren(child, childAnscestors);
-      child.position = idx;
+      child = addAnscestors(child, childAnscestors);
       return child;
     });
   }
@@ -20,8 +31,9 @@ function orderChildren (node, anscestors) {
 
 module.exports = () => fetch(JSON_PATH)
   .then(response => response.json())
-  .then(data => 
-    orderChildren(hierarchy(data), [])
-      .sort((a, b) => b.data.position - a.data.position)
+  .then(data => {
+    data = orderChildren(data);
+    return addAnscestors(hierarchy(data), [])
+      .sort((a, b) => b.data.position > a.data.position)
       .sum(d => d.size)
-  );
+  });
