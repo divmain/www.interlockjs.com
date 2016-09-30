7cbd4892e583ec55c74be3a8b495da108150f953
Dale Bustad <dale@divmain.com>
2016-09-29 19:55:09 -0700

    Enable doc-site offline-browsing.

--- build/ilk-config.js
+++ build/ilk-config.js
@@ -1,6 +1,9 @@
 const path = require("path");
+
 const staticSiteConfig = require("interlock-static-site-config");
 const htmlPrerender = require("interlock-html-prerender");
+const appcache = require("interlock-appcache");
+
 const buildIndex = require("./build-index");
 
 
@@ -24,6 +27,20 @@ module.exports = {
     htmlPrerender(),
     buildIndex({
       dest: "site-index.json"
+    }),
+    appcache({
+      exclude: [
+        /\bwww\.google-analytics\.com\b/
+      ],
+      // Google Fonts APIs will return CSS that references these files.
+      include: [
+        "http://fonts.gstatic.com/s/lato/v11/22JRxvfANxSmnAhzbFH8PgLUuEpTyoUstqEm5AMlJo4.woff2",
+        "http://fonts.gstatic.com/s/alegreyasanssc/v3/AjAmkoP1y0Vaad0UPPR463fFwosrDvhZ0KqLfsf2vpo.woff2",
+        "http://fonts.gstatic.com/s/lato/v11/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2",
+        "http://fonts.gstatic.com/s/lato/v11/MgNNr5y1C_tIEuLEmicLmwLUuEpTyoUstqEm5AMlJo4.woff2",
+        "http://fonts.gstatic.com/s/sourcecodepro/v6/mrl8jkM18OlOQN8JLgasD5bPFduIYtoLzwST68uhz_Y.woff2"
+      ],
+      strip: /(\bindex)?(\.html?)$/
     }),
     ...plugins
   ]
